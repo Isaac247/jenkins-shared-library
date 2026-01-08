0dfe7221b15e6351170cb143c3e887249454ee94
isaac247 <mammanisaac01@gmail.com>
2026-01-08 10:14:44 +0100

    git initialized

---
+++ vars/buildimage.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+def call () {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t izik247/java-app:jma-2.2 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push izik247/java-app:jma-2.2'
+    }
+}
