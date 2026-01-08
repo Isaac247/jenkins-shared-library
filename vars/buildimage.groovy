38ad89911d4f040eceddb74e84910afe14a538e9
isaac247 <mammanisaac01@gmail.com>
2026-01-08 10:59:44 +0100

    updated buildimage groovy script

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -2,8 +2,8 @@
 def call () {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t izik247/java-app:jma-2.2 .'
+        sh 'docker build -t izik247/java-app:jma-2.3 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push izik247/java-app:jma-2.2"
+        sh 'docker push izik247/java-app:jma-2.3"
     }
 }
